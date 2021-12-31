c477037884ce6d4615780a642700ed3e704a293b
bonnk <bonnk25@gmail.com>
2021-12-31 22:30:31 +0200

    second commit

--- chat/urls.py
+++ chat/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    path('',views.home, name='home')
+    path('',views.home, name='home'),
+    path('<str:room>/', views.room, name='room'),
+    path('checkview',views.checkview, name='checkview'),
 ]
